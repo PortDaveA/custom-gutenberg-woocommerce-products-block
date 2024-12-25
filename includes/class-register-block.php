573b43dd763e83ed9075d5df2dd7151fc2aac4ce
Blandford88 <haydave47@gmail.com>
2024-12-25 23:46:01 +0000

    Update class-register-block.php
    Syntax and comment corrections.

--- includes/class-register-block.php
+++ includes/class-register-block.php
@@ -2,30 +2,24 @@
 namespace WooProductsBlock;
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
+    exit; 
 }
 
 class RegisterBlock {
 
-    
-     
-      @var RenderBlock
-     
+    // Instance of RenderBlock (used for dynamic callback).
+    // @var RenderBlock
     private $render_block;
 
     // Constructor.
-      
-      @param RenderBlock $render_block Instance of the RenderBlock class.
-     
+    // @param RenderBlock $render_block Instance of the RenderBlock class.
     public function __construct( RenderBlock $render_block ) {
         $this->render_block = $render_block;
     }
 
-    
-      //Registers the block using register_block_type.
-     
+    // Registers the block using register_block_type.
     public function register() {
-        //  Get dependencies from build/index.asset.php
+        // get dependencies from build/index.asset.php
         $asset_file = include plugin_dir_path( __DIR__ ) . 'build/index.asset.php';
 
         // Register the editor script
@@ -36,10 +30,13 @@ class RegisterBlock {
             $asset_file['version']
         );
 
-        //Register the block
-        register_block_type( 'woo-products-block/products', [
-            'editor_script'   => 'woo-products-block-editor-script',
-            'render_callback' => [ $this->render_block, 'render' ],
-        ] );
+        // Register the block
+        register_block_type(
+            'woo-products-block/products',
+            [
+                'editor_script'   => 'woo-products-block-editor-script',
+                'render_callback' => [ $this->render_block, 'render' ],
+            ]
+        );
     }
 }
