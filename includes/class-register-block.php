733ba0c1051c55efc499c8f64430cfefd87338ed
Blandford88 <haydave47@gmail.com>
2024-12-25 23:41:29 +0000

    Create class-register-block.php
    Registration functions.

---
+++ includes/class-register-block.php
@@ -0,0 +1,45 @@
+<?php
+namespace WooProductsBlock;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+class RegisterBlock {
+
+    
+     
+      @var RenderBlock
+     
+    private $render_block;
+
+    // Constructor.
+      
+      @param RenderBlock $render_block Instance of the RenderBlock class.
+     
+    public function __construct( RenderBlock $render_block ) {
+        $this->render_block = $render_block;
+    }
+
+    
+      //Registers the block using register_block_type.
+     
+    public function register() {
+        //  Get dependencies from build/index.asset.php
+        $asset_file = include plugin_dir_path( __DIR__ ) . 'build/index.asset.php';
+
+        // Register the editor script
+        wp_register_script(
+            'woo-products-block-editor-script',
+            plugins_url( 'build/index.js', dirname( __FILE__ ) ),
+            $asset_file['dependencies'],
+            $asset_file['version']
+        );
+
+        //Register the block
+        register_block_type( 'woo-products-block/products', [
+            'editor_script'   => 'woo-products-block-editor-script',
+            'render_callback' => [ $this->render_block, 'render' ],
+        ] );
+    }
+}
