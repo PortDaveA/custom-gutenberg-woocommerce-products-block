1089139a97d381173b3fe8300387f715db5a5c17
Blandford88 <haydave47@gmail.com>
2024-12-25 23:26:55 +0000

    Create woo-products-block.php
    initialisation

---
+++ woo-products-block.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Custom WooCommerce Products Block
+ * Description: A simple Gutenberg block to display WooCommerce products.
+ * Version: 1.0.0
+ * Author: Anil
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
