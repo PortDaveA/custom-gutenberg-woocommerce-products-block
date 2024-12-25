a7cc0811e84026ddba2c20e19cc8a3c469914f42
Blandford88 <haydave47@gmail.com>
2024-12-25 23:35:28 +0000

    index.asset.php
    dependencies and cache busting

---
+++ build/index.asset.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'dependencies' => array(
+        'react-jsx-runtime',
+        'wp-block-editor',
+        'wp-blocks',
+    ),
+    'version' => '118bef6f94335c2b2c4f', //Created automatically for cache busting purposes, don't worry about it. 
+);
